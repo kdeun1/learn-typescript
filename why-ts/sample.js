f090921b3ba05c343a9d9fb07d2eea469a610cc9
kimdoeun <kdeun1@ex-em.com>
2021-02-15 21:23:58 +0900

    타입스크립트 소개와 배경 ####################### - 타입스크립트란? - 왜 타입스크립트를 쓰면 좋은가요? - 자바스크립트에 타입이 있을 때의 장점 - 자바스크립트를 타입스크립트처럼 코딩하는 방법

---
+++ why-ts/sample.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns {number}
+ */
+function sum(a, b) {
+  return a + b;
+}
+// 30 !== sum(10, '20'); // 1020
+
+sum(10, '20');
