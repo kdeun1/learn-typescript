09bbf9eb911a4ebdff9326b284654edfa9f88b24
kimdoeun <kdeun1@ex-em.com>
2021-04-08 12:58:49 +0900

    강의 후 실습코드 적용 ####################### - enum 코드 실습

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,3 +1,16 @@
+enum Shoes2 {
+  Nike = 11,
+  Adidas = 3,
+  Adidas2 = 3,
+  Adidas3 = 1,
+  NewBalance,
+  NewBalance2,
+  NewBalance23,
+}
+var myShoes2 = Shoes2.Nike;
+console.log(myShoes2); // 0
+
+
 enum Shoes {
   Nike = '나이키',
   Adidas = '아디다스'
@@ -7,12 +20,29 @@ var myShoes = Shoes.Nike;
 console.log(myShoes); // '나이키'
 
 // 예제
+
+function askQuestion(answer: string) {
+  if (answer === 'yes') {
+    console.log('정답입니다.');
+  }
+  if (answer === 'no') {
+    console.log('오답입니다.');
+  }
+}
+
+askQuestion('yes'); // OK
+askQuestion('예스');
+askQuestion('Y');
+askQuestion('Yes');
+
+// ----------------------------
+
 enum Answer {
   Yes = 'Y',
   No = 'N',
 }
 
-function askQuestion(answer: Answer) {
+function askQuestion2(answer: Answer) {
   if (answer === Answer.Yes) {
     console.log('정답입니다');
   }
@@ -20,8 +50,4 @@ function askQuestion(answer: Answer) {
     console.log('오답입니다');
   }
 }
-askQuestion(Answer.Yes);
-askQuestion('Yes');
-// askQuestion('예스');
-// askQuestion('y');
-// askQuestion('Yes');
+askQuestion2(Answer.Yes);
