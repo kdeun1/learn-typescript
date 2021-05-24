051249c374d97acc3a75d6883c1d143100116495
kimdoeun <kdeun1@ex-em.com>
2021-05-24 12:47:46 +0900

    강의 후 실습코드 적용 ####################### 제네릭 코드 실습

--- example/9_type-inference.ts
+++ example/9_type-inference.ts
@@ -1,3 +1,13 @@
+// 타입 추론 기본 1
+var a = 'abc';
+
+function getB(b = 10) {
+  var c = 'hi';
+  return b + c;
+}
+
+getB(10); // '10hi'
+
 var a = 'a';
 
 function logA(a = 'a') {
@@ -5,6 +15,7 @@ function logA(a = 'a') {
   return b;
 }
 
+// 타입 추론 기본 2
 interface Dropdown<T> {
   value: T
   title: string;
