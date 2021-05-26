5b164cb2bb63bbe8eb65b905223ee5ccf7795ab9
kimdoeun <kdeun1@ex-em.com>
2021-05-26 12:52:20 +0900

    강의 후 실습코드 적용 ####################### 제네릭 코드 실습

--- example/9_type-inference.ts
+++ example/9_type-inference.ts
@@ -25,13 +25,60 @@ var items: Dropdown<number> = {
   title: 'a'
 }
 
-interface DetailedDropdown<T> extends Dropdown<T> {
+var shoppingItem: Dropdown<string> = {
+  value: 'abc', // 추론
+  title: 'hello',
+};
+
+// 타입 추론 기본 3
+interface Dropdown<T> {
+  value: T
+  title: string;
+}
+interface DetailedDropdown<K> extends Dropdown<K> {
   description: string;
-  tag: T;
+  tag: K;
 }
-var detailItems: DetailedDropdown<number> = {
-  value: 'hi',
-  title: 'a',
-  description: 'b',
-  tag: 'c'
+
+var detailedItem1: DetailedDropdown<string> = {
+  title: '타이틀1',
+  description: 'desc',
+  value: '값',
+  tag: '태그',
 }
+
+var detailedItem2: DetailedDropdown<number> = {
+  title: '타이틀2',
+  description: 'desc',
+  value: 12,
+  tag: 4534,
+}
+
+// Best Common Type
+var arr = [1, 2, 3];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
